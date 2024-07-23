e93ddc78a53ea7e993891a98897c36239978a88d
VY <veasna.youphon@live.com>
2024-07-23 15:08:17 -0700

    Complete CH3: LogicalOperators

---
+++ CH03 - Operations on Data/06-LogicalOperators/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main(){
+
+    bool a {true};
+    bool b = false;
+    bool c (true);
+
+    std::cout << std::boolalpha;
+    std::cout << "a: " << a << std::endl;
+    std::cout << "b: " << b << std::endl;
+    std::cout << "c: " << c << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Basic AND operations" << std::endl;
+    std::cout << "a && b: " << (a && b) << std::endl;
+    std::cout << "a && c: " << (a && c) << std::endl;
+    std::cout << "a && b && c: " << (a && b && c) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Basic OR operations" << std::endl;
+    std::cout << "a || b: " << (a || b) << std::endl;
+    std::cout << "a || c: " << (a || c) << std::endl;
+    std::cout << "a || b || c: " << (a || b || c) << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Basic NOT operations" << std::endl;
+
+    std::cout << "!a: " << !a << std::endl;
+    std::cout << "!b: " << !b << std::endl;
+    std::cout << "!c: " << !c << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "Combined logical operators" << std::endl;
+    std::cout << "!(a&&b) || c: " <<(!(a&&b) || c) << std::endl;
+
+
+
+    return 0;
+
+}

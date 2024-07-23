d32c7e8f9759fb9c6236f5b9c532ac2ba4256901
VY <veasna.youphon@live.com>
2024-07-23 00:30:43 -0700

    Update CH3: PrefixPostfix-IncrementDecrement

--- CH03 - Operations on Data/03-PrefixPostfix-IncrementDecrement/main.cpp
+++ CH03 - Operations on Data/03-PrefixPostfix-IncrementDecrement/main.cpp
@@ -28,6 +28,21 @@ int main(){
     value = 5;
     std::cout << "The value is (decrementing): " << value-- << std::endl;
     std::cout << "The value is: " << value << std::endl;
+
+    //----------------------------------------------------------------
     
+    std::cout << "--------Prefix increment and decrement---------" << std::endl;
+
+    value = 5;
+    std::cout << "The value is (incrementing): " << ++value << std::endl; //6
+    std::cout << "The value is: " << value <<std::endl; //6
+
+    std::cout << std::endl;
+
+    //Decrement and prefix
+    value = 5;
+    std::cout << "The value is (decrementing): " << --value << std::endl;   //4
+    std::cout << "The value is: " << value << std::endl;    //4
+
     return 0;
 }

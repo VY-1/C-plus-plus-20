ec516d3c3f47a9d488e3016a6d1c4637c7bbb346
VY <veasna.youphon@live.com>
2024-07-23 18:02:18 -0700

    Complete CH05: ForLoop

--- CH05 - Loops/01-ForLoop/main.cpp
+++ CH05 - Loops/01-ForLoop/main.cpp
@@ -80,6 +80,48 @@ int main(){
         std::cout << i << " : I love C++" << std::endl;
     }
     std::cout << "Loop done!" << std::endl;
+
+    //For Loop with Multiple Declarations
+    for (size_t i{0} , x {5}, y{22} ; y > 15 ; ++i , x+=5 , y-=1){
+        std::cout << "i: " << i << ", x : " << x << ", y : " << y << std::endl;
+
+    }
+
+
+    std::cout << "------------Range Based For Loop------------" << std::endl;
+    //Range Based For Loop
+
+    int bag_of_values [] {1,2,3,4,5,6,7,8,9,10}; // This is a collection of integers
+    // The variable value will be assigned a value from the values array on each iteration
+	
+    /*
+    for(size_t i {0} ; i < 10 ; ++i){
+        std::cout << "value : " << bag_of_values[i] << std::endl;
+    }
+    */
+	
+
+    /*
+    for (int value : bag_of_values){
+        //value holds a copy of the current iteration in the whole bag
+        std::cout << " value : " << value << std::endl;
+    }
+    */
+
+
+   //Specify the collection in place
+   /*
+    for (int value : {1,2,3,4,5,6,7,8,9,10}){
+        //value holds a copy of the current iteration in the whole bag
+        std::cout << " value : " << value << std::endl;
+    }
+    */
+
+   //Auto type deduction
+	for (auto value : {1,2,3,4,5,6,7,8,9,10}){
+        //value holds a copy of the current iteration in the whole bag
+        std::cout << " value : " << value << std::endl;
+    }
    
     return 0;
 }

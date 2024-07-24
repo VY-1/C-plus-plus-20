b5c88ea2997ce83a5c3a8cd429ceed305362a68f
VY <veasna.youphon@live.com>
2024-07-23 17:48:52 -0700

    Complete CH05: ForLoop and Update CH04: TernaryOperators

--- CH04 - Flow Control/04-TernaryOperators/main.cpp
+++ CH04 - Flow Control/04-TernaryOperators/main.cpp
@@ -26,6 +26,18 @@ int main(){
     max = (a > b)? a : b; // Ternary operator; same as if true, choose a else choose b; max gets assigned to b since it's false
 	
     std::cout << "max : " << max << std::endl;
+
+    auto max1 = (a > b)? a: 22.5f; //Ternary operation. Auto max1 will assign to float with 22.5
+    std::cout << "max1: " << max1 << std::endl;
+
+    //Ternary Initialization
+    std::cout << std::endl;
+    std::cout << "speed" << std::endl;
+    bool fast = false;
+
+    int speed { fast? 300: 150};    //speed = 150, since fast is false
+
+    std::cout << "The speed is: " << speed << std::endl;
 	
    
     return 0;
